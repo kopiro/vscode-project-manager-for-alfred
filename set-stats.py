9149e471a116c983c3e58164228a18d30a695174
Flavio De Stefano <destefano.flavio@gmail.com>
2023-05-04 00:11:04 +0200

    Adding usage stats

---
+++ set-stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import json
+import sys
+import os
+import consts
+
+if len(sys.argv) > 1 and sys.argv[1]:
+	print(sys.argv)
+
+	# Read stats file
+	stats = {}
+	if os.path.exists(consts.stats_file):
+		try:
+			with open(consts.stats_file, 'r') as file:
+				stats = json.loads(file.read())
+				file.close()
+		except Exception:
+			exit(1)
+
+	project_path = sys.argv[1]
+	if project_path in stats:
+		stats[project_path] += 1
+	else:
+		stats[project_path] = 1
+
+	# Save stats file
+	try:
+		with open(consts.stats_file, 'w') as file:
+			file.write(json.dumps(stats))
+			file.close()
+	except Exception:
+		exit(1)
+    
